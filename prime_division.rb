30d1b6c2b5e6f4d48d8e84a706bf2b2f888065dc
Uwaraba913 <metalcoola5963@gmail.com>
2021-11-30 10:02:53 +0000

    コーディング

--- prime_division.rb
+++ prime_division.rb
@@ -1,5 +1,5 @@
 #入力した値を素因数分解する処理
-require 'prime'
+require 'prime' 
 input = gets.to_i
 judge = Prime.prime_division(input).map {|p, e| [p] * e }.flatten
 puts judge
--- 論理積2.rb
+++ 論理積2.rb
@@ -1,5 +1,5 @@
 #論理積の式2
-A,B = gets.split(" ").map &:to_i
+A,B = gets.split(" ").map &:to_i #2つの数値を入力する
 if A == 1 || B == 1 #AもしくはBが1のときtrue
     answer = 1
 else
