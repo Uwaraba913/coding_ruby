07622afa43af784ec1aeb30ef4b0fdbda6f22456
Uwaraba913 <metalcoola5963@gmail.com>
2021-12-03 11:15:26 +0000

    コーディング

--- ruby.rb
+++ ruby.rb
@@ -1,7 +1,7 @@
 n = 30
 a = 20
 result = [] #空の格納庫
-for i in 1..n
+for i in 1..n #1からn回数分処理を繰り返す
     d = n%i
     if d == 0 && i <= a #答えが余りなし且つaよりも小さい数字
       result << i #3行目のresult[]にtrue条件の数字が格納される
