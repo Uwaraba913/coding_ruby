61b2af4e5cc64ed0c6e9ba0fb90df7553c247a1f
Uwaraba913 <metalcoola5963@gmail.com>
2022-01-09 11:22:15 +0000

    Java勉強

---
+++ AND.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        if(A == B) {
+            System.out.println(A);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
