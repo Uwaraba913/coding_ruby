30466f80aaf4bffdd2e899ef5a9229c78bad350f
Uwaraba913 <metalcoola5963@gmail.com>
2022-01-16 10:58:24 +0000

    Java勉強

---
+++ Progate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a + "/" + b);
+    }
+}
