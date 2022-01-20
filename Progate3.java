c5c23a2f44782e9a697c736e02f6ab52cbaed6bd
Uwaraba913 <metalcoola5963@gmail.com>
2022-01-20 09:06:13 +0000

    Java勉強

---
+++ Progate3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int[] n = new int[] {1, 3, 5, 6, 3, 2, 5, 23, 2};
+        int sum = 0;
+        for(int i = 0; i < n.length; i ++){
+            sum = sum + n[i];
+        }
+        System.out.println(sum);
+    }
+}
